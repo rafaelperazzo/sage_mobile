6ab6ae64218f954b742662556a0d775ab6defd9e
Rafael Perazzo <rafaelperazzo@gmail.com>
2026-04-25 22:58:12 -0300

    fix: Atualizado app para statusbar do expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import '../global.css'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { AuthProvider } from '../src/contexts/AuthContext'
@@ -11,6 +12,7 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
+        <StatusBar style="dark" />
         <AuthProvider>
           <PeriodoProvider>
             <Stack>
--- fix.sh
+++
@@ -1,8 +0,0 @@
-# mobile/fix-native.sh
-#!/bin/bash
-npx expo prebuild --clean
-set -e
-
-# fix build.gradle
-sed -i 's/def projectRoot = .*/&\ndef nodeExecutable = System.getenv("NODE_BINARY") ?: "node"/' android/app/build.gradle
-# ... etc
